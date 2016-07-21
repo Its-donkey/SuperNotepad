e7cafd7a2b8531eac73ba0bf46ea0ce11685a895
Jeff <Jeff@shapirohousehold.com>
2016-07-21 19:17:46 +1000

    Initial

---
+++ SuperNotepad/SuperNotepad/SuperNotepad.Designer.vb
@@ -0,0 +1,288 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmSuperNotepad
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSuperNotepad))
+        Me.mnuTopBar = New System.Windows.Forms.MenuStrip()
+        Me.mnuTopBarFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuTopBarFileNew = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuTopBarFileOpen = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuTopBarFileSave = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuTopBarFileSaveAs = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.mnuTopBarFileClose = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuTopBarEdit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuTopBarEditSelectAll = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuTopBarEditClear = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuTopBarFormat = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuTopBarFormatFont = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuTopBarHelp = New System.Windows.Forms.ToolStripMenuItem()
+        Me.staBottomBar = New System.Windows.Forms.StatusStrip()
+        Me.staBottomBarBtnFont = New System.Windows.Forms.ToolStripSplitButton()
+        Me.staBottomBarBtnFontRight = New System.Windows.Forms.ToolStripMenuItem()
+        Me.staBottomBarBtnFontCentre = New System.Windows.Forms.ToolStripMenuItem()
+        Me.staBottomBarBtnFontLeft = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblVerticalBar = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lblBottomBar = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lblVerticalBar1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lblTime = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.tmrTimeUpdate = New System.Windows.Forms.Timer(Me.components)
+        Me.rtfWritingArea = New System.Windows.Forms.RichTextBox()
+        Me.SupportToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuTopBar.SuspendLayout()
+        Me.staBottomBar.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'mnuTopBar
+        '
+        Me.mnuTopBar.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuTopBarFile, Me.mnuTopBarEdit, Me.mnuTopBarFormat, Me.mnuTopBarHelp})
+        Me.mnuTopBar.Location = New System.Drawing.Point(0, 0)
+        Me.mnuTopBar.Name = "mnuTopBar"
+        Me.mnuTopBar.Size = New System.Drawing.Size(749, 24)
+        Me.mnuTopBar.TabIndex = 0
+        Me.mnuTopBar.Text = "MenuStrip1"
+        '
+        'mnuTopBarFile
+        '
+        Me.mnuTopBarFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuTopBarFileNew, Me.mnuTopBarFileOpen, Me.mnuTopBarFileSave, Me.mnuTopBarFileSaveAs, Me.ToolStripSeparator2, Me.mnuTopBarFileClose})
+        Me.mnuTopBarFile.Name = "mnuTopBarFile"
+        Me.mnuTopBarFile.Size = New System.Drawing.Size(37, 20)
+        Me.mnuTopBarFile.Text = "&File"
+        '
+        'mnuTopBarFileNew
+        '
+        Me.mnuTopBarFileNew.Name = "mnuTopBarFileNew"
+        Me.mnuTopBarFileNew.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.N), System.Windows.Forms.Keys)
+        Me.mnuTopBarFileNew.Size = New System.Drawing.Size(152, 22)
+        Me.mnuTopBarFileNew.Text = "&New"
+        '
+        'mnuTopBarFileOpen
+        '
+        Me.mnuTopBarFileOpen.Name = "mnuTopBarFileOpen"
+        Me.mnuTopBarFileOpen.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
+        Me.mnuTopBarFileOpen.Size = New System.Drawing.Size(152, 22)
+        Me.mnuTopBarFileOpen.Text = "&Open"
+        '
+        'mnuTopBarFileSave
+        '
+        Me.mnuTopBarFileSave.Name = "mnuTopBarFileSave"
+        Me.mnuTopBarFileSave.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.mnuTopBarFileSave.Size = New System.Drawing.Size(152, 22)
+        Me.mnuTopBarFileSave.Text = "&Save"
+        '
+        'mnuTopBarFileSaveAs
+        '
+        Me.mnuTopBarFileSaveAs.Name = "mnuTopBarFileSaveAs"
+        Me.mnuTopBarFileSaveAs.ShortcutKeys = System.Windows.Forms.Keys.F12
+        Me.mnuTopBarFileSaveAs.ShowShortcutKeys = False
+        Me.mnuTopBarFileSaveAs.Size = New System.Drawing.Size(152, 22)
+        Me.mnuTopBarFileSaveAs.Text = "Sa&ve As"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(149, 6)
+        '
+        'mnuTopBarFileClose
+        '
+        Me.mnuTopBarFileClose.Name = "mnuTopBarFileClose"
+        Me.mnuTopBarFileClose.ShortcutKeys = CType((System.Windows.Forms.Keys.Alt Or System.Windows.Forms.Keys.F4), System.Windows.Forms.Keys)
+        Me.mnuTopBarFileClose.Size = New System.Drawing.Size(152, 22)
+        Me.mnuTopBarFileClose.Text = "&Close"
+        '
+        'mnuTopBarEdit
+        '
+        Me.mnuTopBarEdit.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuTopBarEditSelectAll, Me.mnuTopBarEditClear})
+        Me.mnuTopBarEdit.Name = "mnuTopBarEdit"
+        Me.mnuTopBarEdit.Size = New System.Drawing.Size(39, 20)
+        Me.mnuTopBarEdit.Text = "&Edit"
+        '
+        'mnuTopBarEditSelectAll
+        '
+        Me.mnuTopBarEditSelectAll.Name = "mnuTopBarEditSelectAll"
+        Me.mnuTopBarEditSelectAll.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.A), System.Windows.Forms.Keys)
+        Me.mnuTopBarEditSelectAll.Size = New System.Drawing.Size(175, 22)
+        Me.mnuTopBarEditSelectAll.Text = "Select &All"
+        '
+        'mnuTopBarEditClear
+        '
+        Me.mnuTopBarEditClear.Name = "mnuTopBarEditClear"
+        Me.mnuTopBarEditClear.ShortcutKeys = CType(((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Shift) _
+            Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.mnuTopBarEditClear.Size = New System.Drawing.Size(175, 22)
+        Me.mnuTopBarEditClear.Text = "Clear"
+        '
+        'mnuTopBarFormat
+        '
+        Me.mnuTopBarFormat.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuTopBarFormatFont})
+        Me.mnuTopBarFormat.Name = "mnuTopBarFormat"
+        Me.mnuTopBarFormat.Size = New System.Drawing.Size(57, 20)
+        Me.mnuTopBarFormat.Text = "For&mat"
+        '
+        'mnuTopBarFormatFont
+        '
+        Me.mnuTopBarFormatFont.Name = "mnuTopBarFormatFont"
+        Me.mnuTopBarFormatFont.Size = New System.Drawing.Size(98, 22)
+        Me.mnuTopBarFormatFont.Text = "&Font"
+        '
+        'mnuTopBarHelp
+        '
+        Me.mnuTopBarHelp.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SupportToolStripMenuItem})
+        Me.mnuTopBarHelp.Name = "mnuTopBarHelp"
+        Me.mnuTopBarHelp.Size = New System.Drawing.Size(44, 20)
+        Me.mnuTopBarHelp.Text = "&Help"
+        '
+        'staBottomBar
+        '
+        Me.staBottomBar.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.staBottomBarBtnFont, Me.lblVerticalBar, Me.lblBottomBar, Me.lblVerticalBar1, Me.lblTime})
+        Me.staBottomBar.Location = New System.Drawing.Point(0, 479)
+        Me.staBottomBar.Name = "staBottomBar"
+        Me.staBottomBar.Size = New System.Drawing.Size(749, 22)
+        Me.staBottomBar.TabIndex = 2
+        Me.staBottomBar.Text = "StatusStrip1"
+        '
+        'staBottomBarBtnFont
+        '
+        Me.staBottomBarBtnFont.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.staBottomBarBtnFont.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.staBottomBarBtnFontRight, Me.staBottomBarBtnFontCentre, Me.staBottomBarBtnFontLeft})
+        Me.staBottomBarBtnFont.Image = CType(resources.GetObject("staBottomBarBtnFont.Image"), System.Drawing.Image)
+        Me.staBottomBarBtnFont.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.staBottomBarBtnFont.Name = "staBottomBarBtnFont"
+        Me.staBottomBarBtnFont.Size = New System.Drawing.Size(51, 20)
+        Me.staBottomBarBtnFont.Text = "Align"
+        Me.staBottomBarBtnFont.ToolTipText = "Change Font"
+        '
+        'staBottomBarBtnFontRight
+        '
+        Me.staBottomBarBtnFontRight.Name = "staBottomBarBtnFontRight"
+        Me.staBottomBarBtnFontRight.Size = New System.Drawing.Size(109, 22)
+        Me.staBottomBarBtnFontRight.Text = "&Right"
+        '
+        'staBottomBarBtnFontCentre
+        '
+        Me.staBottomBarBtnFontCentre.Name = "staBottomBarBtnFontCentre"
+        Me.staBottomBarBtnFontCentre.Size = New System.Drawing.Size(109, 22)
+        Me.staBottomBarBtnFontCentre.Text = "&Centre"
+        '
+        'staBottomBarBtnFontLeft
+        '
+        Me.staBottomBarBtnFontLeft.Name = "staBottomBarBtnFontLeft"
+        Me.staBottomBarBtnFontLeft.Size = New System.Drawing.Size(109, 22)
+        Me.staBottomBarBtnFontLeft.Text = "&Left"
+        '
+        'lblVerticalBar
+        '
+        Me.lblVerticalBar.Name = "lblVerticalBar"
+        Me.lblVerticalBar.Size = New System.Drawing.Size(22, 17)
+        Me.lblVerticalBar.Text = "  |  "
+        '
+        'lblBottomBar
+        '
+        Me.lblBottomBar.Name = "lblBottomBar"
+        Me.lblBottomBar.Size = New System.Drawing.Size(548, 17)
+        Me.lblBottomBar.Spring = True
+        '
+        'lblVerticalBar1
+        '
+        Me.lblVerticalBar1.Name = "lblVerticalBar1"
+        Me.lblVerticalBar1.Size = New System.Drawing.Size(22, 17)
+        Me.lblVerticalBar1.Text = "  |  "
+        '
+        'lblTime
+        '
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(91, 17)
+        Me.lblTime.Text = "CURRENT_TIME"
+        '
+        'tmrTimeUpdate
+        '
+        Me.tmrTimeUpdate.Enabled = True
+        '
+        'rtfWritingArea
+        '
+        Me.rtfWritingArea.AcceptsTab = True
+        Me.rtfWritingArea.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rtfWritingArea.Location = New System.Drawing.Point(0, 28)
+        Me.rtfWritingArea.Name = "rtfWritingArea"
+        Me.rtfWritingArea.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical
+        Me.rtfWritingArea.ShowSelectionMargin = True
+        Me.rtfWritingArea.Size = New System.Drawing.Size(749, 448)
+        Me.rtfWritingArea.TabIndex = 3
+        Me.rtfWritingArea.Text = ""
+        '
+        'SupportToolStripMenuItem
+        '
+        Me.SupportToolStripMenuItem.Name = "SupportToolStripMenuItem"
+        Me.SupportToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1
+        Me.SupportToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SupportToolStripMenuItem.Text = "&Support"
+        '
+        'frmSuperNotepad
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(749, 501)
+        Me.Controls.Add(Me.rtfWritingArea)
+        Me.Controls.Add(Me.staBottomBar)
+        Me.Controls.Add(Me.mnuTopBar)
+        Me.MainMenuStrip = Me.mnuTopBar
+        Me.MinimumSize = New System.Drawing.Size(765, 540)
+        Me.Name = "frmSuperNotepad"
+        Me.Text = "SuperNotepad"
+        Me.mnuTopBar.ResumeLayout(False)
+        Me.mnuTopBar.PerformLayout()
+        Me.staBottomBar.ResumeLayout(False)
+        Me.staBottomBar.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents mnuTopBar As MenuStrip
+    Friend WithEvents staBottomBar As StatusStrip
+    Friend WithEvents mnuTopBarFile As ToolStripMenuItem
+    Friend WithEvents mnuTopBarFileOpen As ToolStripMenuItem
+    Friend WithEvents mnuTopBarFileSave As ToolStripMenuItem
+    Friend WithEvents mnuTopBarFormat As ToolStripMenuItem
+    Friend WithEvents mnuTopBarFormatFont As ToolStripMenuItem
+    Friend WithEvents mnuTopBarFileNew As ToolStripMenuItem
+    Friend WithEvents lblBottomBar As ToolStripStatusLabel
+    Friend WithEvents lblTime As ToolStripStatusLabel
+    Friend WithEvents tmrTimeUpdate As Timer
+    Friend WithEvents lblVerticalBar1 As ToolStripStatusLabel
+    Friend WithEvents lblVerticalBar As ToolStripStatusLabel
+    Friend WithEvents mnuTopBarEdit As ToolStripMenuItem
+    Friend WithEvents mnuTopBarEditSelectAll As ToolStripMenuItem
+    Friend WithEvents mnuTopBarFileSaveAs As ToolStripMenuItem
+    Friend WithEvents mnuTopBarFileClose As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents mnuTopBarEditClear As ToolStripMenuItem
+    Friend WithEvents staBottomBarBtnFontRight As ToolStripMenuItem
+    Friend WithEvents staBottomBarBtnFontCentre As ToolStripMenuItem
+    Friend WithEvents staBottomBarBtnFontLeft As ToolStripMenuItem
+    Friend WithEvents staBottomBarBtnFont As ToolStripSplitButton
+    Friend WithEvents rtfWritingArea As RichTextBox
+    Friend WithEvents mnuTopBarHelp As ToolStripMenuItem
+    Friend WithEvents SupportToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ SuperNotepad/SuperNotepad/SuperNotepad.vb
@@ -0,0 +1,211 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Public Class frmSuperNotepad
+    Private activeFile As String = Nothing
+    Private changeMade As Boolean = False
+    Private textAlignment As String
+    Private Sub frmSuperNotepad_Load(sender As Object, e As EventArgs) Handles Me.Load
+        staBottomBar.Items("lblTime").Text = DateTime.Now.ToShortTimeString
+    End Sub
+    Private Sub mnuTopBarFileOpen_Click(sender As Object, e As EventArgs) Handles mnuTopBarFileOpen.Click
+        Dim openStream As Stream = Nothing
+        Dim dlgOpenFileDialog As New OpenFileDialog()
+
+        dlgOpenFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+        dlgOpenFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+        dlgOpenFileDialog.FilterIndex = 1
+        dlgOpenFileDialog.RestoreDirectory = True
+
+        If dlgOpenFileDialog.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Try
+                openStream = dlgOpenFileDialog.OpenFile()
+                activeFile = dlgOpenFileDialog.FileName()
+
+                If (openStream IsNot Nothing) Then
+                    Using sr As New StreamReader(openStream)
+                        Dim line As String
+                        ' Read the stream to a string and write the string to the console.
+                        line = sr.ReadToEnd()
+                        rtfWritingArea.Text = line
+                    End Using
+                End If
+            Catch Ex As Exception
+                MessageBox.Show("Cannot read file from disk. Original error: " & Ex.Message)
+            Finally
+                ' Check this again, since we need to make sure we didn't throw an exception on open.
+                If (openStream IsNot Nothing) Then
+                    openStream.Close()
+                End If
+            End Try
+        End If
+
+        lblBottomBar.Visible = True
+        lblBottomBar.Text = "Editing: " & dlgOpenFileDialog.SafeFileName
+
+    End Sub
+    Private Sub mnuTopBarFileSave_Click(sender As Object, e As EventArgs) Handles mnuTopBarFileSave.Click
+        If activeFile IsNot Nothing Then
+            Dim sw As New System.IO.StreamWriter(activeFile)
+            sw.Write(rtfWritingArea.Text)
+            sw.Close()
+        Else
+            mnuTopBarFileSaveAs_Click(sender, e)
+        End If
+    End Sub
+    Private Sub mnuTopBarFileSaveAs_Click(sender As Object, e As EventArgs) Handles mnuTopBarFileSaveAs.Click
+        Dim saveStream As Stream = Nothing
+        Dim dlgSaveFileDialog As New SaveFileDialog()
+
+        dlgSaveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+        dlgSaveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+        dlgSaveFileDialog.FilterIndex = 1
+        dlgSaveFileDialog.RestoreDirectory = True
+
+        If dlgSaveFileDialog.ShowDialog() = DialogResult.OK Then
+            saveStream = dlgSaveFileDialog.OpenFile()
+            activeFile = dlgSaveFileDialog.FileName()
+            If (saveStream IsNot Nothing) Then
+                Dim sw As New System.IO.StreamWriter(saveStream)
+                sw.Write(rtfWritingArea.Text)
+                sw.Close()
+            End If
+        End If
+
+        lblBottomBar.Visible = True
+        lblBottomBar.Text = "Saved to: " & dlgSaveFileDialog.FileName
+    End Sub
+
+
+    Private Sub mnuTopBarFormatFont_Click(sender As Object, e As EventArgs) Handles mnuTopBarFormatFont.Click
+        Dim dlgFontDialog As New FontDialog()
+        Dim previousSatus As String = lblBottomBar.Text
+
+        dlgFontDialog.ShowColor = True
+        dlgFontDialog.Color = rtfWritingArea.ForeColor
+        dlgFontDialog.Font = rtfWritingArea.Font
+
+        If dlgFontDialog.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Try
+                rtfWritingArea.Font = dlgFontDialog.Font
+                rtfWritingArea.ForeColor = dlgFontDialog.Color
+            Catch ex As Exception
+                MessageBox.Show("Unable to apply the selected font: " & ex.Message)
+            End Try
+        End If
+
+        lblBottomBar.Visible = True
+        lblBottomBar.Text = "Font changed to: " & rtfWritingArea.Font.Name & " - " & rtfWritingArea.ForeColor.Name
+
+    End Sub
+
+    Private Sub mnuTopBarFileNew_Click(sender As Object, e As EventArgs) Handles mnuTopBarFileNew.Click
+        If rtfWritingArea.Text IsNot "" Or changeMade Then
+            If MsgBox("Are you sure?", MsgBoxStyle.OkCancel, "New SuperNotepad") = System.Windows.Forms.DialogResult.OK Then
+                rtfWritingArea.Text = ""
+            End If
+        End If
+
+        lblBottomBar.Visible = False
+        changeMade = False
+    End Sub
+
+    Private Sub tmrTimeUpdate_Tick(sender As Object, e As EventArgs) Handles tmrTimeUpdate.Tick
+        staBottomBar.Items("lblTime").Text = DateTime.Now.ToShortTimeString
+    End Sub
+
+    Private Sub mnuTopBarEditSelectAll_Click(sender As Object, e As EventArgs) Handles mnuTopBarEditSelectAll.Click
+        rtfWritingArea.SelectAll()
+    End Sub
+
+    Private Sub rtfWritingArea_TextChanged(sender As Object, e As EventArgs)
+        changeMade = True
+    End Sub
+
+    Private Sub frmSuperNotepad_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        If changeMade Then
+            Dim saveOnClose As Integer = MsgBox("Would you like to save the changes made?", MsgBoxStyle.YesNoCancel, "Save changes")
+            If saveOnClose = System.Windows.Forms.DialogResult.Yes Then
+                If activeFile IsNot Nothing Then
+                    Dim sw As New System.IO.StreamWriter(activeFile)
+                    sw.Write(rtfWritingArea.Text)
+                    sw.Close()
+                Else
+                    mnuTopBarFileSaveAs_Click(sender, e)
+                End If
+            ElseIf saveOnClose = System.Windows.Forms.DialogResult.No Then
+                Application.Exit()
+            ElseIf saveOnClose = System.Windows.Forms.DialogResult.Cancel Then
+                e.Cancel = True
+            End If
+        End If
+    End Sub
+
+    Private Sub mnuTopBarFileClose_Click(sender As Object, e As EventArgs) Handles mnuTopBarFileClose.Click
+        Close()
+    End Sub
+
+    Private Sub frmSuperNotepad_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        rtfWritingArea.Height = (Me.Height - 88)
+        rtfWritingArea.Width = (Me.Width - 16)
+    End Sub
+
+    Private Sub mnuTopBarEditClear_Click(sender As Object, e As EventArgs) Handles mnuTopBarEditClear.Click
+        Dim clearText As Integer = MsgBox("Are you sure?", MsgBoxStyle.YesNo, "Clear Text")
+        If clearText = System.Windows.Forms.DialogResult.Yes Then
+            rtfWritingArea.Clear()
+        ElseIf clearText = System.Windows.Forms.DialogResult.No Then
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub staBottomBarBtnFontLeft_Click(sender As Object, e As EventArgs) Handles staBottomBarBtnFontLeft.Click
+        If textAlignment IsNot "Left" Then
+            rtfWritingArea.SelectAll()
+            rtfWritingArea.SelectionAlignment = HorizontalAlignment.Left
+            textAlignment = "Left"
+        End If
+
+        staBottomBarBtnFontLeft.Checked = True
+        staBottomBarBtnFontRight.Checked = False
+        staBottomBarBtnFontCentre.Checked = False
+
+    End Sub
+
+    Private Sub staBottomBarBtnFontCentre_Click(sender As Object, e As EventArgs) Handles staBottomBarBtnFontCentre.Click
+        If textAlignment IsNot "Centre" Then
+            rtfWritingArea.SelectAll()
+            rtfWritingArea.SelectionAlignment = HorizontalAlignment.Center
+            textAlignment = "Centre"
+        End If
+
+        staBottomBarBtnFontLeft.Checked = False
+        staBottomBarBtnFontRight.Checked = False
+        staBottomBarBtnFontCentre.Checked = True
+
+    End Sub
+
+    Private Sub staBottomBarBtnFontRight_Click(sender As Object, e As EventArgs) Handles staBottomBarBtnFontRight.Click
+        If textAlignment IsNot "Right" Then
+            rtfWritingArea.SelectAll()
+            rtfWritingArea.SelectionAlignment = HorizontalAlignment.Right
+            textAlignment = "Right"
+        End If
+
+        staBottomBarBtnFontLeft.Checked = False
+        staBottomBarBtnFontRight.Checked = True
+        staBottomBarBtnFontCentre.Checked = False
+    End Sub
+
+    Private Sub staBottomBarBtnFont_ButtonClick(sender As Object, e As EventArgs) Handles staBottomBarBtnFont.ButtonClick
+        staBottomBarBtnFont.ShowDropDown()
+    End Sub
+
+    Private Sub SupportToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SupportToolStripMenuItem.Click
+        Dim pdf As Byte() = My.Resources.helpFile
+        Using tmp As New FileStream("SuperNotepad_help.pdf", FileMode.Create)
+            tmp.Write(pdf, 0, pdf.Length)
+        End Using
+        Process.Start("SuperNotepad_help.pdf")
+    End Sub
+End Class
+
